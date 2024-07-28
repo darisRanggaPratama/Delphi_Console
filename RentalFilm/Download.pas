fdf1f145e80d220d10cb5b22ab5dd229faca1cb2
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2024-07-28 13:56:11 +0700

    initial commit

--- RentalFilm/Download.pas
+++ RentalFilm/Download.pas
@@ -10,8 +10,10 @@ uses
 type
   Tform_download = class(TForm)
     btnExport: TSpeedButton;
+    btnExportCSV: TSpeedButton;
 
     procedure btnExportClick(Sender: TObject);
+    procedure btnExportCSVClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -36,7 +38,47 @@ var
   ProcessInfo: TProcessInformation;
 begin
   // Path ke skrip Python Anda
-  PythonScriptPath := ExtractFilePath(Application.ExeName) + 'connect.py';
+  PythonScriptPath := ExtractFilePath(Application.ExeName) + 'export2Excel.py';
+
+  // Pastikan file skrip Python ada
+  if not FileExists(PythonScriptPath) then
+  begin
+    ShowMessage('File skrip Python tidak ditemukan!');
+    Exit;
+  end;
+
+  // Buat command untuk menjalankan skrip Python
+  Command := 'python "' + PythonScriptPath + '"';
+
+  // Inisialisasi struktur StartupInfo
+  FillChar(StartInfo, SizeOf(TStartupInfo), 0);
+  StartInfo.cb := SizeOf(TStartupInfo);
+
+  // Jalankan command
+  if CreateProcess(nil, PChar(Command), nil, nil, False, 0, nil, nil, StartInfo,
+    ProcessInfo) then
+  begin
+    // Tunggu proses selesai
+    WaitForSingleObject(ProcessInfo.hProcess, INFINITE);
+
+    // Tutup handle
+    CloseHandle(ProcessInfo.hProcess);
+    CloseHandle(ProcessInfo.hThread);
+
+    ShowMessage('Ekspor ke Excel selesai!');
+  end
+  else
+    ShowMessage('Gagal menjalankan skrip Python!');
+end;
+
+procedure Tform_download.btnExportCSVClick(Sender: TObject);
+var
+  PythonScriptPath, Command: string;
+  StartInfo: TStartupInfo;
+  ProcessInfo: TProcessInformation;
+begin
+  // Path ke skrip Python Anda
+  PythonScriptPath := ExtractFilePath(Application.ExeName) + 'export2CSV.py';
 
   // Pastikan file skrip Python ada
   if not FileExists(PythonScriptPath) then
